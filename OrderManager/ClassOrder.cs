3db05d27ca78e9342bc289dae16da1332f50990d
Dasha23451 <daspsh@mail.ru>
2024-07-08 23:18:30 +0300

    Добавила класс Order и убрала дублирование кода

---
+++ OrderManager/ClassOrder.cs
@@ -0,0 +1,15 @@
+﻿public class Order
+{
+    public string UserName { get; private set; }
+    public string ProductName { get; private set; }
+    public int CountProduct { get; private set; }
+    public string Address { get; private set; }
+
+    public Order( string userName, string productName, int count, string address )
+    {
+        UserName = userName;
+        ProductName = productName;
+        CountProduct = count;
+        Address = address;
+    }
+}
--- OrderManager/Program.cs
+++ OrderManager/Program.cs
@@ -1,77 +1,66 @@
 ﻿class Program
 {
-    static string ReadUserName()
-    {
-        Console.Write( "Enter your name: " );
-        string userName = Console.ReadLine();
-        while ( string.IsNullOrWhiteSpace( userName ) )
-        {
-            userName = Console.ReadLine();
-        }
-
-        return userName;
-    }
-
-    static string ReadProductName()
-    {
-        Console.Write( "Enter product name: " );
-        string productName = Console.ReadLine();
-        while ( string.IsNullOrWhiteSpace( productName ) )
-        {
-            productName = Console.ReadLine();
-        }
-
-        return productName;
-    }
-
     static int GetCountProduct()
     {
         Console.Write( "Enter product quantity: " );
         string count = Console.ReadLine();
+        int goods_quantity;
         while ( string.IsNullOrWhiteSpace( count ) )
         {
             count = Console.ReadLine();
         }
-        int goods_quantity = Int32.Parse( count );
-
+        while ( !( int.TryParse( count, out goods_quantity ) && goods_quantity > 0 ) )
+        {
+            Console.WriteLine( "Quantity must be a number and greater than 0. Enter a value" );
+            count = Console.ReadLine();
+        }
         return goods_quantity;
     }
 
-    static string GetDeliveryAddress()
+    static string GetStringData( string message )
     {
-        Console.Write( "Enter delivery address: " );
-        string address = Console.ReadLine();
-        while ( string.IsNullOrWhiteSpace( address ) )
+        Console.Write( message );
+        string value = Console.ReadLine();
+        while ( string.IsNullOrWhiteSpace( value ) )
         {
-            address = Console.ReadLine();
+            Console.WriteLine( "String cannot be empty. Enter a value:" );
+            value = Console.ReadLine();
         }
 
-        return address;
+        return value;
     }
 
-    static void OrderConfirmation( string name, string product, int count, string address )
+    static void OrderConfirmation( Order order )
     {
-        Console.WriteLine( $"Hello, {name}, you ordered {count} {product} to address {address}, is that right? (y/n)" );
+        Console.WriteLine( $"Hello, {order.UserName}, you ordered {order.CountProduct} will be delivered {order.ProductName} to {order.Address}, is that right? (y/n)" );
         string confirmation = Console.ReadLine();
         if ( confirmation.ToLower() == "y" )
         {
             DateTime todays_date = DateTime.Today;
             DateTime delivery_date = todays_date.AddDays( 3 );
 
-            Console.WriteLine( $"{name}! Your order {product} in quantity {count} decorated! Expect delivery to {address}  {delivery_date:d}" );
+            Console.WriteLine( $"{order.UserName}! Your order {order.ProductName} in quantity {order.CountProduct} decorated! Expect delivery to {order.Address}  {delivery_date:d}" );
         }
         else
         {
             Console.WriteLine( "Your order is not completed. Please check your input and try again." );
         }
+
     }
+    static Order CreateOrder()
+    {
+        string name = GetStringData( "Enter your name: " );
+        string product = GetStringData( "Enter product name: " );
+        int count = GetCountProduct();
+        string address = GetStringData( "Enter delivery address: " );
+        Order userOrder = new Order( name, product, count, address );
+        return userOrder;
+    }
+
     static void Main()
     {
         Console.WriteLine( "Welcome! Please enter order details:" );
-        string userName = ReadUserName();
-        string productName = ReadProductName();
-        int countProduct = GetCountProduct();
-        string deliveryAddress = GetDeliveryAddress();
-        OrderConfirmation( userName, productName, countProduct, deliveryAddress );
+        Order userOrder = CreateOrder();
+        OrderConfirmation( userOrder );
     }
 }
